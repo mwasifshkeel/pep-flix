e96f2c42767f5bfb2c5f52050925f4ac8b66eacd
Muhammad Wasif Shakeel <mshakeel.bsds23seecs@seecs.edu.pk>
2024-08-16 23:42:09 +0500

    Added placeholder transparency upon focus

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -20,7 +20,7 @@ export default function Main(props) {
     <main className="mb-8 flex-1 bg-zinc-700">
       <div>
         <div className='flex items-center justify-center py-10'>
-          <input className='p-2 leading-8 bg-zinc-800 b text-center items-center rounded-xl outline-none focus:outline-1 focus:outline-amber-200 caret-white text-white w-[300px] md:w-[500px] lg:w-[700px]' type="text" placeholder="Enter Movie Name..." value={search} onChange={handleSearchChange} />
+          <input className='focus:placeholder-transparent p-2 leading-8 bg-zinc-800 b text-center items-center rounded-xl outline-none focus:outline-1 focus:outline-amber-200 caret-white text-white w-[300px] md:w-[500px] lg:w-[700px]' type="text" placeholder="Enter Movie Name..." value={search} onChange={handleSearchChange} />
         </div>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center p-3 gap-6 md:gap-8 lg:gap-10 '>
           {filteredMovies.map((movie, movieIndex) => {
