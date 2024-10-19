dbb9117aa22f5d67bb61808e51e5bff1cf2d1373
Muhammad Wasif Shakeel <mshakeel.bsds23seecs@seecs.edu.pk>
2024-10-19 13:42:25 +0500

    Changed API from FreeTest to TMDB

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,68 @@
-import Header from "./components/Header"
-import {useState,useEffect} from "react"
+import Header from "./components/Header";
+import { useState, useEffect } from "react";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 
 function App() {
-  const [movieData,setMovieData] = useState(null);
+  const [movieData, setMovieData] = useState(null);
   const [queueItems, setQueueItems] = useState(() => {
-    const savedQueueItems = localStorage.getItem('queueItems');
+    const savedQueueItems = localStorage.getItem("queueItems");
     return savedQueueItems ? JSON.parse(savedQueueItems) : [];
   });
+  const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
-  useEffect(()=>{
-    async function fetchAPIData(){
-      //https://freetestapi.com/api/v1/movies
-      const url = 'https://freetestapi.com/api/v1/movies'
-      const today = (new Date()).toDateString();
+  useEffect(() => {
+    async function fetchAPIData() {
+      const url = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+      const today = new Date().toDateString();
       const localKey = `MOVIE-${today}`;
 
-      if(localStorage.getItem(localKey)){
+      if (localStorage.getItem(localKey)) {
         const apiData = JSON.parse(localStorage.getItem(localKey));
-        setMovieData(apiData);
+        setMovieData(apiData.results);
         console.log(apiData);
-        console.log("API DATA AT 0")
-        console.log(apiData[0])
-        return
+        return;
       }
 
-      localStorage.clear()
-      
+      localStorage.clear();
 
-      try{
+      try {
         const res = await fetch(url);
         const apiData = await res.json();
-        localStorage.setItem(localKey,JSON.stringify(apiData));
+        console.log(apiData);
+        localStorage.setItem(localKey, JSON.stringify(apiData));
         setMovieData(apiData);
         console.log(apiData);
-      }catch(err){
+      } catch (err) {
         console.log(err.message);
         setMovieData(null);
       }
     }
 
     fetchAPIData();
-  },[]);
+  }, []);
 
   useEffect(() => {
-    localStorage.setItem('queueItems', JSON.stringify(queueItems));
+    localStorage.setItem("queueItems", JSON.stringify(queueItems));
   }, [queueItems]);
 
   return (
     <div className="flex flex-col min-h-screen">
-      <Header setQueueItems={setQueueItems} queueItems={queueItems}/>
-      {movieData ? 
-      (<Main queueItems={queueItems} setQueueItems={setQueueItems} movieData={movieData} />) : (<div className="bg-zinc-700 loadingState"><i className="fa-solid fa-gear"></i></div>)
-      }
+      <Header setQueueItems={setQueueItems} queueItems={queueItems} />
+      {movieData ? (
+        <Main
+          queueItems={queueItems}
+          setQueueItems={setQueueItems}
+          movieData={movieData}
+        />
+      ) : (
+        <div className="bg-zinc-700 loadingState">
+          <i className="fa-solid fa-gear"></i>
+        </div>
+      )}
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,52 +1,151 @@
-import React, { useState } from 'react'
-import { Tooltip } from 'react-tooltip'
-import 'react-tooltip/dist/react-tooltip.css'
+import React from "react";
+import { Tooltip } from "react-tooltip";
+import "react-tooltip/dist/react-tooltip.css";
 
 export default function MovieCard(props) {
-    const {movie,queueItems,setQueueItems} = props
+  const { movie, queueItems, setQueueItems } = props;
 
-    const addToQueue = ()=> {
-      setQueueItems(prevQueueItems=>[...prevQueueItems,movie]);
-    }
+  const addToQueue = () => {
+    setQueueItems((prevQueueItems) => [...prevQueueItems, movie]);
+  };
 
-    const removeFromQueue = ()=> {
-      const newQueueItems = queueItems.filter(queueMovie => queueMovie.title !== movie.title)
-      setQueueItems(newQueueItems);
-    }
+  const removeFromQueue = () => {
+    const newQueueItems = queueItems.filter(
+      (queueMovie) => queueMovie.title !== movie.title
+    );
+    setQueueItems(newQueueItems);
+  };
+
+  const getLanguageName = (code) => {
+    const languageMap = {
+      en: "English",
+      ko: "Korean",
+      es: "Spanish",
+      fr: "French",
+      de: "German",
+      ja: "Japanese",
+      zh: "Chinese",
+      tl: "Filipino",
+      sv: "Swedish",
+    };
+    return languageMap[code] || "Unknown Language";
+  };
+
+  const getCountryName = (code) => {
+    const countryMap = {
+      en: "United States",
+      ko: "South Korea",
+      es: "Spain",
+      fr: "France",
+      de: "Germany",
+      ja: "Japan",
+      zh: "China",
+      tl: "Philippines",
+      sv: "Sweden",
+    };
+    return countryMap[code] || "Unknown Country";
+  };
+
+  const thresholdLength = 150;
 
   return (
-    <div className='relative h-[430px] rounded-2xl flex flex-col max-w-[400px] gap-3 sm:gap-6 bg-zinc-600 w-full p-3 text-slate-100'>
-            <div className='flex justify-between text-lg sm:text-xl lg:text-2xl font-bold items-start'>
-              <h2>{movie.title}</h2>
-              <h2 className='text-slate-400'>{movie.year}</h2>
-            </div>
-            <div>
-              <h3 className='text-sm sm:text-base uppercase'>Country: <span className='text-md sm:text-lg text-gray-400'>{movie.country}</span></h3>
-              <h3 className='text-sm sm:text-base uppercase'>Language: <span className='text-md sm:text-lg text-gray-400'>{movie.language}</span></h3>
-              <br />
-              <h4 className='text-sm sm:text-base uppercase'>PLOT</h4>
-            </div>
-            <div className='flex-1'>
-              <p className='text-base sm:text-md text-gray-400'>{movie.plot}</p>
-            </div>
-            <div className='flex gap-2 flex-col'>
-              <h3 className='text-sm sm:text-base uppercase'>GENRES</h3>
-              <div className='flex gap-2 flex-wrap text-xs sm:text-sm'>
-                {movie.genre.map((genre, genreIndex) => {
-                    return (
-                    <div key={genreIndex} index={genreIndex} className='pointer-events-none select-none rounded-2xl flex items-center justify-center bg-zinc-800 text-gray-300 w-[90px] h-[40px] border-amber-200 border-[1px]'>
-                        <p>{genre}</p>
-                    </div>
-                    )
-                })}
+    <div className="relative h-[430px] rounded-2xl flex flex-col max-w-[400px] gap-3 sm:gap-6 bg-zinc-600 w-full p-3 text-slate-100">
+      <div className="flex justify-between text-lg sm:text-xl lg:text-2xl font-bold items-start">
+        <h2>{movie.title}</h2>
+        <h2 className="text-slate-400">
+          {new Date(movie.release_date).getFullYear()}
+        </h2>
+      </div>
+      <div>
+        <h3 className="text-sm sm:text-base uppercase">
+          Country:{" "}
+          <span className="text-md sm:text-lg text-gray-400">
+            {getCountryName(movie.original_language)}
+          </span>
+        </h3>
+        <h3 className="text-sm sm:text-base uppercase">
+          Language:{" "}
+          <span className="text-md sm:text-lg text-gray-400">
+            {getLanguageName(movie.original_language)}
+          </span>
+        </h3>
+        <br />
+        <h4 className="text-sm sm:text-base uppercase">PLOT</h4>
+      </div>
+      <div className="flex-1 overflow-hidden">
+        <div
+          className={`plot-container ${
+            movie.overview.length > thresholdLength ? "scrollable" : ""
+          }`}
+        >
+          <p className="text-base sm:text-md text-gray-400">{movie.overview}</p>
+        </div>
+      </div>
+      <div className="flex gap-2 flex-col">
+        <h3 className="text-sm sm:text-base uppercase">GENRES</h3>
+        <div className="flex gap-2 flex-wrap text-xs sm:text-sm">
+          {movie.genre_ids.map((genreId, genreIndex) => {
+            const genreName = getGenreName(genreId);
+            return (
+              <div
+                key={genreIndex}
+                className="pointer-events-none select-none rounded-2xl flex items-center justify-center bg-zinc-800 text-gray-300 w-[90px] h-[40px] border-amber-200 border-[1px]"
+              >
+                <p>{genreName}</p>
               </div>
-            </div>
-            <div className='absolute top-0 right-0 translate-x-2 -translate-y-2 opacity-30'>
-              <button onClick={queueItems.some(item => item.title === movie.title)?removeFromQueue:addToQueue}>
-              <i className={`fa-solid text-2xl hover:text-slate-400 ${queueItems.some(item => item.title === movie.title)? 'fa-circle-minus addqueue' : 'fa-circle-plus removequeue'}`} data-tooltip-id="queue-status" data-tooltip-content={queueItems.indexOf(movie)!=-1?"Remove from queue":"Add to queue"} />
-              </button>
-            </div>
-            <Tooltip id="queue-status" />
-          </div>
-  )
+            );
+          })}
+        </div>
+      </div>
+      <div className="absolute top-0 right-0 translate-x-2 -translate-y-2 opacity-30">
+        <button
+          onClick={
+            queueItems.some((item) => item.title === movie.title)
+              ? removeFromQueue
+              : addToQueue
+          }
+        >
+          <i
+            className={`fa-solid text-2xl hover:text-slate-400 ${
+              queueItems.some((item) => item.title === movie.title)
+                ? "fa-circle-minus addqueue"
+                : "fa-circle-plus removequeue"
+            }`}
+            data-tooltip-id="queue-status"
+            data-tooltip-content={
+              queueItems.indexOf(movie) !== -1
+                ? "Remove from queue"
+                : "Add to queue"
+            }
+          />
+        </button>
+      </div>
+      <Tooltip id="queue-status" />
+    </div>
+  );
+}
+
+const genreMap = {
+  12: "Adventure",
+  16: "Animation",
+  27: "Horror",
+  28: "Action",
+  35: "Comedy",
+  36: "History",
+  53: "Thriller",
+  80: "Crime",
+  99: "Documentary",
+  18: "Drama",
+  14: "Fantasy",
+  10751: "Family",
+  9648: "Mystery",
+  10749: "Romance",
+  878: "Sci-Fi",
+  10770: "TV Movie",
+  10752: "War",
+  37: "Western",
+};
+
+function getGenreName(id) {
+  return genreMap[id] || "Unknown Genre";
 }
